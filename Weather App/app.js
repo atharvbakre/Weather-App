726f1782b547ca9121a23ccfc666481169f467f1
atharvbakre <44296079+atharvbakre@users.noreply.github.com>
2020-10-26 15:40:26 +0530

    Updates

--- Weather App/app.js
+++ Weather App/app.js
@@ -1,16 +1,16 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const forecast = require('./utils/forecast')
+const forecast = require('./src/utils/forecast')
 
 // express
 const app = express();
 const port = process.env.PORT || 3000
 
 // paths for express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, './public')
+const viewsPath = path.join(__dirname, './templates/views')
+const partialsPath = path.join(__dirname, './templates/partials')
 
 // set up handlebars engine views and location
 app.set('view engine', 'hbs')
